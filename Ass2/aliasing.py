6a4ae705ee367514b86beb0fa126f157bef7aaf0
Andreas Vibeto <Andreas Vibeto>
2016-09-16 00:14:14 +0200

    Finished assignment 2

--- Ass2/aliasing.py
+++ Ass2/aliasing.py
@@ -5,21 +5,26 @@ from scipy import misc
 
 
 def downsample(image):
+	#Removes every second row and column
 	return image[0::2,0::2]
-
 
 def convolution(image, kernel):
 	image_size = len(image)
 	kernel_size = kernel.shape[0]
 
+	#Add padding to both kernel and image. Padding size is twice the
+	#image size to ensure enough padding.
 	padded_kernel = np.zeros((2*image_size, 2*image_size))
 	padded_kernel[(image_size)-(kernel_size/2):(image_size)+(kernel_size/2)+1,
 			      (image_size)-(kernel_size/2):(image_size)+(kernel_size/2)+1] = kernel
 	padded_image = np.zeros((2*image_size, 2*image_size))
 	padded_image[image_size:3*image_size,image_size:3*image_size] = image
 
+	#Fourier transform image and kernel
 	fourier_image = np.fft.fft2(padded_image)
 	fourier_kernel = np.fft.fft2(padded_kernel)
+	
+	#Convolution
 	fourier_convolved = fourier_image * fourier_kernel
 	image_convolved = np.fft.ifft2(fourier_convolved).real
 
@@ -33,9 +38,6 @@ if __name__ == "__main__":
 	kernel_low = (1.0 / 9) * np.ones((3,3))
 	smooth_image = downsample(convolution(image, kernel_low))
 	down_image = downsample(image)
-
-
-	
 	
 	_, ax = plt.subplots(1,3, figsize=(27,15))
 	ax[0].imshow(image, cmap = plt.cm.Greys_r)
@@ -45,3 +47,6 @@ if __name__ == "__main__":
 	ax[2].imshow(down_image, cmap = plt.cm.Greys_r)
 	ax[2].set_axis_off()
 	plt.show()
+
+	misc.imsave('task2_2/smoothened_image.png', smooth_image)
+	misc.imsave('task2_2/downsized_image.png', down_image)
--- Ass2/filtering.py
+++ Ass2/filtering.py
@@ -8,14 +8,19 @@ def convolution(image, kernel):
 	image_size = len(image)
 	kernel_size = kernel.shape[0]
 
+	#Add padding to both kernel and image. Padding size is twice the
+	#image size to ensure enough padding.
 	padded_kernel = np.zeros((2*image_size, 2*image_size))
 	padded_kernel[(image_size)-(kernel_size/2):(image_size)+(kernel_size/2)+1,
 			      (image_size)-(kernel_size/2):(image_size)+(kernel_size/2)+1] = kernel
 	padded_image = np.zeros((2*image_size, 2*image_size))
 	padded_image[image_size:3*image_size,image_size:3*image_size] = image
 
+	#Fourier transform image and kernel
 	fourier_image = np.fft.fft2(padded_image)
 	fourier_kernel = np.fft.fft2(padded_kernel)
+	
+	#Convolution
 	fourier_convolved = fourier_image * fourier_kernel
 	image_convolved = np.fft.ifft2(fourier_convolved).real
 
@@ -23,24 +28,12 @@ def convolution(image, kernel):
 	return image_convolved[:image_size, :image_size]
 											
 def hybrid(image1, image2):
-	kernel_low = (1.0 / 9) * np.ones((9,9))
-	
-	#kernel_low = np.array([[(1.0/16),(1.0/8),(1.0/16)],
-	#				   	   [(1.0/8),(1.0/4),(1.0/8)],
-	#				   	   [(1.0/16),(1.0/8),(1.0/16)]])
-	
-	#kernel_low = np.array([[0,(1.0/8),0],
-	#					   [(1.0/8),(1.0/2),(1.0/8)],
-	#					   [0,(1.0/8),0]])
-
+	kernel_low = np.array([[(1.0/16),(1.0/8),(1.0/16)],
+					   	   [(1.0/8),(1.0/4),(1.0/8)],
+					   	   [(1.0/16),(1.0/8),(1.0/16)]])
 	kernel_high = np.array([[0,-1,0],
 					   	    [-1,4,-1],
 					        [0,-1,0]])
-	#kernel_low = (1.0/273)*np.array([[1,4,7,4,1],
-	#			   [4,16,26,16,4],
-	#			   [7,26,41,26,7],
-	#			   [4,16,26,16,4],
-	#			   [1,4,7,4,1]])
 
 	low_image = convolution(image1, kernel_low)
 	high_image = convolution(image2, kernel_high)
@@ -55,20 +48,12 @@ def hybrid(image1, image2):
 	plt.show()
 	
 	return high_image + low_image
-	
 
 def task_2_1_a():
 	image = misc.imread('./images/lake.tiff', True)
 	image_size = len(image)
 	
-	#kernel_low = (1.0 / 9) * np.ones((3,3))
-	
-	kernel_low = (1/273)*np.array([[1,4,7,4,1],
-				   [4,16,26,16,4],
-				   [7,26,41,26,7],
-				   [4,16,26,16,4],
-				   [1,4,7,4,1]])
-
+	kernel_low = (1.0 / 9) * np.ones((3,3))
 	kernel_high = np.array([[0,-1,0],
 					   	    [-1,4,-1],
 					        [0,-1,0]])
@@ -76,6 +61,7 @@ def task_2_1_a():
 	convolved_low = convolution(image, kernel_low)
 	convolved_high = convolution(image, kernel_high)
 	
+	#Plot images
 	_, ax = plt.subplots(1,3, figsize=(27,15))
 	ax[0].imshow(image, cmap = plt.cm.Greys_r)
 	ax[0].set_axis_off()
@@ -83,13 +69,14 @@ def task_2_1_a():
 	ax[1].set_axis_off()
 	ax[2].imshow(convolved_high, cmap = plt.cm.Greys_r)
 	ax[2].set_axis_off()
-	#plt.show()
+	plt.show()
 
-
+	#Produce the amplitude of the transform
 	image_amp = np.log(np.absolute(np.fft.fft2(image).real)+1)
 	convolved_amp_low = np.log(np.absolute(np.fft.fft2(convolved_low).real)+1)
 	convolved_amp_high = np.log(np.absolute(np.fft.fft2(convolved_high).real)+1)
 	
+	#Plot images
 	_, ax = plt.subplots(1,3, figsize=(27,15))
 	ax[0].imshow(np.fft.fftshift(image_amp), cmap = plt.cm.Greys_r)
 	ax[0].set_axis_off()
@@ -99,10 +86,9 @@ def task_2_1_a():
 	ax[2].set_axis_off()
 	plt.show()
 
-	misc.imsave('task2_1_a/filtered/original.png', image)
-	misc.imsave('task2_1_a/filtered/low_pass.png', convolved_low)
-	misc.imsave('task2_1_a/filtered/high_pass.png', convolved_high)
-	
+	misc.imsave('task2_1_a/filtered/filtered_original.png', image)
+	misc.imsave('task2_1_a/filtered/filtered_low_pass.png', convolved_low)
+	misc.imsave('task2_1_a/filtered/filtered_high_pass.png', convolved_high)
 	misc.imsave('task2_1_a/amp/original_amp.png',
 				np.fft.fftshift(image_amp))
 	misc.imsave('task2_1_a/amp/low_pass_amp.png',
@@ -121,6 +107,7 @@ def task_2_1_b():
 	convolved_image = convolution(image, kernel_high)
 	sharpened_image = image + convolved_image
 	
+	#Plot images
 	_, ax = plt.subplots(1,2, figsize=(27,15))
 	ax[0].imshow(image, cmap = plt.cm.Greys_r)
 	ax[0].set_axis_off()
@@ -141,8 +128,9 @@ def task_2_1_c():
 	plt.imshow(hybrid_image, cmap = plt.cm.Greys_r)
 	#plt.show()
 	
+	misc.imsave('hybrid_image.png', hybrid_image)
+	
 if __name__ == "__main__":	
-	#task_2_1_a()
+	task_2_1_a()
 	#task_2_1_b()
-	task_2_1_c()
-
+	#task_2_1_c()
